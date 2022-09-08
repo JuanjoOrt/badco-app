815dc9c19bae344f5950ee4011e1c07094e98b7e
Juanjo Ortiz <juanjoseortizgamez@gmail.com>
2022-09-08 08:43:36 +0200

    removing useAsyncstorage

--- src/hooks/useAsyncStorage.jsx
+++
@@ -1,9 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
-
-export default function useAsyncStorage () {
-  const setItem = async (key, value) => { await AsyncStorage.setItem(key, value) }
-  const getItem = async (key) => await AsyncStorage.getItem(key)
-  const removeItem = async (key) => await AsyncStorage.removeItem(key)
-
-  return { setItem, getItem, removeItem }
-}
